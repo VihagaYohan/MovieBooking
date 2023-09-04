2e0aec20bbe92bf22a36968074ca1a4e2999cc09
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-09-04 18:07:52 +0530

    Initial Commit

---
+++ MovieBooking/MovieBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookingApp.swift
+//  MovieBooking
+//
+//  Created by Yohan on 2023-09-04.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
