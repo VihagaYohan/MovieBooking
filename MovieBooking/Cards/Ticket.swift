719714f4322b8694f737be8cd379072d42b0292b
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-09-05 05:33:12 +0530

    Added ticket

--- MovieBooking/Cards/Ticket.swift
+++ MovieBooking/Cards/Ticket.swift
@@ -12,6 +12,8 @@ struct Ticket: View {
     @State var title: String = "Thor"
     @State var subTitle: String = "Love And Thunder"
     @State var top: String = "thor-top"
+    @State var bottom: String = "thor-bottom"
+    @Binding var height: CGFloat
     
     var gradient: [Color] = [Color("cyan"), Color("cyan").opacity(0), Color("cyan").opacity(0)]
     
@@ -41,6 +43,68 @@ struct Ticket: View {
                     .stroke(LinearGradient(colors: gradient, startPoint: .topLeading, endPoint: .bottomTrailing), style: StrokeStyle(lineWidth: 2))
             }
             .cornerRadius(40, corners: [.topLeft, .topRight])
+            
+            Spacer(minLength: height)
+            
+            VStack(spacing: 10.0) {
+                Line()
+                    .stroke(style: StrokeStyle(lineWidth: 2, dash: [7]))
+                    .frame(width: 200, height: 1)
+                    .opacity(0.6)
+                
+                HStack(spacing: 20.0) {
+                    HStack(spacing: 4.0){
+                        Text("Date:")
+                            .fontWeight(Font.Weight.medium)
+                            .foregroundColor(Color("lightPurple"))
+                        
+                        Text("April 23")
+                            .foregroundColor(Color.black)
+                    }
+                    
+                    HStack(spacing: 20.0) {
+                        Text("Time")
+                            .fontWeight(Font.Weight.medium)
+                            .foregroundColor(Color("lightPurple"))
+                        
+                        Text("6 p.m")
+                            .foregroundColor(Color.black)
+                    }
+                }
+                
+                HStack(spacing: 20.0) {
+                    HStack(spacing: 4.0){
+                        Text("Row")
+                            .fontWeight(Font.Weight.medium)
+                            .foregroundColor(Color("lightPurple"))
+                        
+                        Text("2")
+                            .foregroundColor(Color.black)
+                    }
+                    
+                    HStack(spacing: 20.0){
+                        Text("Seats")
+                            .fontWeight(Font.Weight.medium)
+                            .foregroundColor(Color("lightPurple"))
+                        
+                        Text("9, 10")
+                            .foregroundColor(Color.black)
+                    }
+                }
+                Image("code")
+            }
+            .frame(width: 250, height: 135, alignment: .top)
+            .background(.ultraThinMaterial)
+            .background(
+                Image(bottom)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            )
+            .mask(
+                Image(bottom)
+                    .resizable()
+                    .aspectRatio(contentMode: ContentMode.fill)
+            )
         }
         .frame(height: 460)
         .font(.footnote)
@@ -50,6 +114,6 @@ struct Ticket: View {
 
 struct Ticket_Previews: PreviewProvider {
     static var previews: some View {
-        Ticket()
+        Ticket(height: .constant(0))
     }
 }
---
+++ MovieBooking/Cards/Tickets.swift
@@ -0,0 +1,20 @@
+//
+//  Tickets.swift
+//  MovieBooking
+//
+//  Created by Yohan on 2023-09-05.
+//
+
+import SwiftUI
+
+struct Tickets: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Tickets_Previews: PreviewProvider {
+    static var previews: some View {
+        Tickets()
+    }
+}
