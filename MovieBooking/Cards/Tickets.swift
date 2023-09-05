42e3e00f1f85064bb5582c931641483e7ded54e4
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-09-05 05:57:20 +0530

    Ticket stack

--- MovieBooking/Cards/Tickets.swift
+++ MovieBooking/Cards/Tickets.swift
@@ -8,13 +8,78 @@
 import SwiftUI
 
 struct Tickets: View {
+    @State var tickets: [TicketModel] = [
+        TicketModel(image:"thor", title:"Thor", subTitle: "Love and Thunder", top:"thor-top", bottom: "thor-bottom"),
+        TicketModel(image: "panther", title:"Black Panther", subTitle: "Wakanda Forever", top: "panther-top", bottom: "panther-bottom"),
+        TicketModel(image: "scarlet", title: "Doctor Strange", subTitle: "In the multiverse of madness", top: "scarlet-top", bottom: "scarlet-bottom")
+    ]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            ForEach(tickets) { ticket in
+                InfiniteStackView(tickets: $tickets, ticket: ticket)
+            }
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
     }
 }
 
 struct Tickets_Previews: PreviewProvider {
     static var previews: some View {
         Tickets()
+    }
+}
+
+struct InfiniteStackView: View {
+    @Binding var tickets: [TicketModel]
+    
+    @GestureState var isDragging: Bool = false
+    @State var offset: CGFloat = .zero
+    @State var height: CGFloat = 0
+    
+    var ticket: TicketModel
+    
+    var body: some View {
+        VStack {
+            Ticket(
+                title: ticket.title, subTitle: ticket.subTitle, top: ticket.top, bottom: ticket.bottom, height: $height)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .zIndex(Double(CGFloat(tickets.count) - getIndex()))
+        .rotationEffect(getIndex() == 1 ? .degrees(-6) : .degrees(0))
+        .rotationEffect(getIndex() == 2 ? .degrees(6) : .degrees(0))
+        .scaleEffect(getIndex() == 0 ? 1 : 0.9)
+        .offset(x: getIndex() == 1 ? -40 : 0)
+        .offset(x: getIndex() == 2 ? 40 : 0)
+        .offset(x: offset)
+        .gesture(
+            DragGesture()
+                .updating($isDragging, body: {_, out, _ in
+                    out = true
+                })
+                .onChanged({ value in
+                    var translation = value.translation.width
+                    translation = tickets.first?.id == ticket.id ? translation : 0
+                    translation = isDragging ? translation : 0
+                    
+                    withAnimation(.easeInOut(duration: 0.3)) {
+                        offset = translation
+                    }
+                })
+                .onEnded {value in
+                    withAnimation(.easeInOut(duration: 0.5)) {
+                        offset = .zero
+                    }
+                }
+        )
+    }
+    
+    func getIndex() -> CGFloat {
+        let index = tickets.firstIndex { ticket in
+            return self.ticket.id == ticket.id
+        } ?? 0
+        
+        return CGFloat(index)
     }
 }
